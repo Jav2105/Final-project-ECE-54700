91baef1b6f790c4542d8c6f7b8a2e603e1d12f3a
Jav2105 <44086545+Jav2105@users.noreply.github.com>
2025-12-09 23:58:11 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -4,18 +4,24 @@ class Network:
         self.n_servers = n_servers  # List of Server instances
         self.model = model
     
+    '''
     def broadcast_ping(self):
         for server in self.servers:
             for other_server in self.servers:
                 if server != other_server:
                     server.ping(other_server)
-
+    '''
+                    
 class Server:
     def __init__(self, server_id, model, buffer_size):
         self.server_id = server_id
         self.model = model
         self.buffer_size = buffer_size
         self.n_packets = 0
+
+    def update(self):
+        # Update server state, e.g., process packets
+        pass
 
     def ping(self, other_server):
         # Simulate pinging another server
@@ -24,11 +30,23 @@ class Server:
     def can_accept_task(self, task_load):
         return (self.current_load + task_load) <= self.capacity
 
-    def assign_task(self, task_load):
-        if self.can_accept_task(task_load):
-            self.current_load += task_load
-            return True
-        return False
-
     def release_task(self, task_load):
         self.current_load = max(0, self.current_load - task_load)
+
+class MemorylessServer(Server):
+    def __init__(self, server_id, model, buffer_size):
+        super().__init__(server_id, model, buffer_size)
+        # Additional initialization for memoryless server
+
+    def update(self):
+        # Specific update logic for memoryless server
+        pass
+
+class DeterministicServer(Server):
+    def __init__(self, server_id, model, buffer_size):
+        super().__init__(server_id, model, buffer_size)
+        # Additional initialization for deterministic server
+
+    def update(self):
+        # Specific update logic for deterministic server
+        pass
