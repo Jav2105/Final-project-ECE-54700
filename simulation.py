8100e5c5cbd5fd5d86b14446257f00b59ac9ffd4
Jav2105 <44086545+Jav2105@users.noreply.github.com>
2025-12-10 00:04:01 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Network:
     def __init__(self, n_servers, model):
@@ -13,11 +14,13 @@ class Network:
     '''
                     
 class Server:
-    def __init__(self, server_id, model, buffer_size):
+    def __init__(self, server_id, model, buffer_size, arrival_rate, departure_rate):
         self.server_id = server_id
         self.model = model
         self.buffer_size = buffer_size
         self.n_packets = 0
+        self.arrival_rate = arrival_rate
+        self.departure_rate = departure_rate
 
     def update(self):
         # Update server state, e.g., process packets
@@ -27,15 +30,12 @@ class Server:
         # Simulate pinging another server
         print(f"Pinging server {other_server.server_id} from server {self.server_id}")
 
-    def can_accept_task(self, task_load):
-        return (self.current_load + task_load) <= self.capacity
-
-    def release_task(self, task_load):
-        self.current_load = max(0, self.current_load - task_load)
+    def can_accept_packet(self):
+        return (self.n_packets + 1) <= self.buffer_size
 
 class MemorylessServer(Server):
-    def __init__(self, server_id, model, buffer_size):
-        super().__init__(server_id, model, buffer_size)
+    def __init__(self, server_id, model, buffer_size, arrival_rate, departure_rate):
+        super().__init__(server_id, model, buffer_size, arrival_rate, departure_rate)
         # Additional initialization for memoryless server
 
     def update(self):
@@ -43,8 +43,8 @@ class MemorylessServer(Server):
         pass
 
 class DeterministicServer(Server):
-    def __init__(self, server_id, model, buffer_size):
-        super().__init__(server_id, model, buffer_size)
+    def __init__(self, server_id, model, buffer_size, arrival_rate, departure_rate):
+        super().__init__(server_id, model, buffer_size, arrival_rate, departure_rate)
         # Additional initialization for deterministic server
 
     def update(self):
